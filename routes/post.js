65ffd29f74d65c56ec01a1e7b5db9dcd37e6f513
rabiussunnyc3x@gmail.com <rabiussunnyc3x@gmail.com>
2022-04-24 07:08:07 +0600

    folde structure

---
+++ routes/post.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/home/:page')
+router.post('/create-biodata')
+router.post('/update-biodata')
+router.get('/delete-biodata/:id')
+
+router.get('/featured/')
+router.get('/post-featured/:id')
+router.get('/delete-featured/:id')
+
+router.get('/post-favorites/:id/:bioid')
+router.get('/delete-favorites/:id/:bioid')
+router.get('/favorites/:id/:page')
